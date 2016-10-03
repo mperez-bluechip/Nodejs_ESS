15e58de4d2f776d3af8f3988fb7ac855023e333b
Manny Perez <mannyperez@Mannys-MacBook-Pro.local>
2016-10-03 15:19:22 -0500

    Added exec and spawn jpprocesses

--- BenFranklin.js
+++ BenFranklin.js
@@ -1,15 +1,15 @@
-var EventEmitter = require('events').EventEmitter;
-var util = require('util');
-var Person = function(name){
-  this.name = name;
-};
-
-util.inherits(Person, EventEmitter);
+var Person = require('./lib/Person');
 
 var ben = new Person("Ben Franklin");
+var george = new Person("George Washington");
 
 ben.on('speak', function(said){
+  console.log(`${this.name} -> ${said}`);
+});
+
+george.on('speak', function(said){
   console.log(`${this.name}: ${said}`);
 });
 
 ben.emit('speak', "You may delay, but time will not.");
+george.emit('speak', "It is far better to be alone, than to be in bad company.");
