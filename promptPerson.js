8a2fe3f8cd4938c1619e7e1a20efbc52bcc4c670
Manny Perez <mannyperez@Mannys-MacBook-Pro.local>
2016-10-04 10:26:28 -0500

    Added sample https request, sample http request using Nodes create server object method

--- promptPerson.js
+++ promptPerson.js
@@ -10,17 +10,18 @@ var realPerson = {
 rl.question("What is the name of a real person? ", function(answer){
 
   realPerson.name = answer;
-  fs.writeFileSync(realPerson.name + ".md", `${realPerson.name}\n==================\n\n`)
+  var stream = fs.createWriteStream(realPerson.name + ".md");
+  stream.write(`${realPerson.name}\n==================\n\n`);
   rl.setPrompt(`What would ${realPerson.name} say? `);
   rl.prompt();
 
   rl.on('line', function(saying){
-    realPerson.sayings.push(saying.trim());
-
-    fs.appendFile(realPerson.name + ".md", `* ${saying.trim()} \n`);
     if(saying.toLowerCase().trim() === 'exit'){
+      stream.close();
       rl.close();
     }else{
+      realPerson.sayings.push(saying.trim());
+      stream.write(`* ${saying.trim()} \n`);
       rl.setPrompt(`What else would ${realPerson.name} say? ('exit' to leave) `);
       rl.prompt();
     }
---
+++ streams.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+var stream = fs.createReadStream("./chat.log", "UTF-8");
+
+var data = "";
+
+stream.once("data", function(){
+  console.log("\n\n\n");
+  console.log("Started reading file");
+  console.log("\n\n\n");
+});
+
+stream.on("data", function(chunk){
+  process.stdout.write(`  chunk: ${chunk.length} |`);
+  data += chunk;
+});
+
+stream.on("end", function(){
+  console.log("\n\n\n");
+  console.log(`Finished  reading file ${data.length}`);
+  console.log("\n\n\n");
+});
