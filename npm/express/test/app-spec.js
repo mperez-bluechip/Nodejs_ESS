ba6e592bccc2d1e77b1061e92731d112473431ee
Manny Perez <threedyou@gmail.com>
2016-10-06 15:53:15 -0500

    Added supertest for test http endpoints

---
+++ npm/express/test/app-spec.js
@@ -0,0 +1,47 @@
+var request = require('supertest'),
+    expect = require('chai').expect,
+    rewire = require('rewire'),
+    app = rewire('../app');
+
+describe("Dictionary App", function () {
+
+    it("Loads the home page", function(done){
+      request(app).get("/").expect(200).end(done);
+    });
+
+    describe("Dictionary API", function () {
+
+        beforeEach(function () {
+
+        	this.defs = [
+                {
+                    term: "One",
+                    defined: "Term One Defined"
+                },
+                {
+                    term: "Two",
+                    defined: "Term Two Defined"
+                }
+            ];
+
+            app.__set__("skierTerms", this.defs);
+        });
+
+        it("GETS dictionary-api", function(done){
+          request(app).get("/dictionary-api").expect(200).end(done);
+        });
+
+        it("POSTS dictionary-api", function(done){
+          request(app).post("/dictionary-api").send({
+            "term": "Three",
+            "defined": "Term Three Defined"
+          }).expect(200).end(done);
+        });
+
+        it("DELETES dictionary-api", function(done){
+          request(app).delete("/dictionary-api/One").expect(200).end(done);
+        });
+
+    });
+
+});
