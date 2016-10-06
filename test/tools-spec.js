b99157af7ff232aeb274a959228c64d189020b32
Manny Perez <threedyou@gmail.com>
2016-10-06 14:27:28 -0500

    Added mocha testing file and nock dependecy and test files

--- test/tools-spec.js
+++ test/tools-spec.js
@@ -1,5 +1,6 @@
-var expect = require("chai").expect;
-var tools = require("../lib/tools");
+var expect = require("chai").expect,
+    tools = require("../lib/tools"),
+    nock = require("nock");
 
 describe("Tools", function(){
 
@@ -12,10 +13,16 @@ describe("Tools", function(){
   	});
   });
 
-  describe("loadWiki()", function(){
+  describe("loadWiki()", function(done){
+    before(function(){
+      nock("https://en.wikipedia.org")
+      .get("/wiki/Abraham_Lincoln")
+      .reply(200, "Mock Abraham Lincoln Page");
+    });
     it("Load Abraham Lincoln's Wikipedia page");
     tools.loadWiki({ first: "Abraham", last: "Lincoln"}, function(html){
-      expect(html).to.be.ok;
+      expect(html).to.equal("Mock Abraham Lincoln Page");
+      done();
     });
   });
 });
